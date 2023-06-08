3289987d82a48534652b9750ede7c0ea78cc171a
yoshida-takeshi <56309857+yoshida-takeshi@users.noreply.github.com>
2023-06-08 22:47:55 +0900

    Create google_trans.py

---
+++ google_trans.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import openpyxl
+from googletrans import Translator
+
+translator = Translator()
+
+args = sys.argv
+
+# Excel読み込み
+path=args[1]
+print(path)
+
+wb = openpyxl.load_workbook(path)
+sheet = wb.worksheets[0]
+
+# 1行ごとに翻訳
+for row in range(1, sheet.max_row + 1):
+    en = sheet.cell(row=row, column=1).value
+    if en!=None:
+        ja = translator.translate(text=en, src="en", dest="ja")
+        print(en)
+        print("  =>",ja.text)
+
+        sheet.cell(row=row, column=2).value = ja.text
+        sheet.cell(row=row, column=2).alignment = openpyxl.styles.Alignment(wrapText=True)
+
+wb.save(path)
