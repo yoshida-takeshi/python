bbe8648b79af97990fa5763f6deee51b2c7df419
yoshida-takeshi <56309857+yoshida-takeshi@users.noreply.github.com>
2023-06-09 10:40:56 +0900

    Update google_trans.py

--- google_trans.py
+++ google_trans.py
@@ -2,6 +2,7 @@
 
 #https://qiita.com/taashi/items/07bf75201a074e208ae5
 
+import os
 import sys
 import openpyxl
 from googletrans import Translator
@@ -11,10 +12,12 @@ translator = Translator()
 args = sys.argv
 
 # Excel読み込み
-path=args[1]
-print(path)
+path_in=args[1]
+tmpf=os.path.splitext(path_in)
+path_out=tmpf[0] + '_trans' +tmpf[1]
+print(path_in)
 
-wb = openpyxl.load_workbook(path)
+wb = openpyxl.load_workbook(path_in)
 sheet = wb.worksheets[0]
 
 # 1行ごとに翻訳
@@ -28,4 +31,4 @@ for row in range(1, sheet.max_row + 1):
         sheet.cell(row=row, column=2).value = ja.text
         sheet.cell(row=row, column=2).alignment = openpyxl.styles.Alignment(wrapText=True)
 
-wb.save(path)
+wb.save(path_out)
