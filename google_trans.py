f1b8a9c32bad4051f7153cfe4981b7e35f26d6d2
yoshida-takeshi <56309857+yoshida-takeshi@users.noreply.github.com>
2023-06-10 16:10:31 +0900

    Update google_trans.py

--- google_trans.py
+++ google_trans.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 
-#https://qiita.com/taashi/items/07bf75201a074e208ae5
+#https://stmtk358.hatenablog.com/entry/2021/09/13/083728
+#pip install googletrans==4.0.0-rc1
 
 import os
 import sys
 import openpyxl
 from googletrans import Translator
 
-translator = Translator()
-
 args = sys.argv
 
-# Excel読み込み
+lang_src="en"
+lang_dest="ja"
+
+translator = Translator()
+
+#ファイル名
 path_in=args[1]
 tmpf=os.path.splitext(path_in)
 path_out=tmpf[0] + '_trans' +tmpf[1]
-print(path_in)
+print("input: %s\n" %(path_in))
 
+#Excel読み込み
 wb = openpyxl.load_workbook(path_in)
 sheet = wb.worksheets[0]
 
@@ -24,11 +29,13 @@ sheet = wb.worksheets[0]
 for row in range(1, sheet.max_row + 1):
     en = sheet.cell(row=row, column=1).value
     if en!=None:
-        ja = translator.translate(text=en, src="en", dest="ja")
-        print(en)
-        print("  =>",ja.text)
+        ja = translator.translate(text=en, src=lang_src, dest=lang_dest)
+        print(" %3d: %s" % (row,en))
+        print("      => %s" %(ja.text))
 
         sheet.cell(row=row, column=2).value = ja.text
-        sheet.cell(row=row, column=2).alignment = openpyxl.styles.Alignment(wrapText=True)
+        sheet.cell(row=row, column=2).alignment = openpyxl.styles.Alignment(wrapText=False)
 
+#Excel出力
+print("\noutput: %s" % (path_out))
 wb.save(path_out)
